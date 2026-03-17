5d1de3084e091a9415fc6d8635b6da8ce47aaa63
Aavash232311 <aavash3150@gmail.com>
2026-03-17 14:07:39 -0500

    Safe sql table, creation for re-running python script

--- pipeline-orchestrator.PythonService/src/query/create_skill_table.sql
+++ pipeline-orchestrator.PythonService/src/query/create_skill_table.sql
@@ -1,7 +1,7 @@
 -- This is one of my project for solving, some problems but aslo 
 -- I am trying to relate with CSC330 class so I am writing queries here
 
-CREATE TABLE SKILL_TABLE (
+CREATE TABLE IF NOT EXISTS SKILL_TABLE (
     id UUID PRIMARY KEY,
     preferredLabel VARCHAR(255) NOT NULL,
     altLabels VARCHAR(255) NOT NULL,
